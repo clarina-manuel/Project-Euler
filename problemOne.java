51e7e65e2b916e357ffa86730f6d28e7bbd53c3e
clarina-manuel <clarina.t.manuel@gmail.com>
2024-11-07 14:00:36 -0800

    Solved #5

--- problemOne.java
+++ problemOne.java
@@ -1,1 +1,22 @@
-//problem 1
+//Smallest Multiple
+
+int multiple = 2520;
+boolean isMultiple = false;
+
+while (isMultiple != true) {
+  multiple += 2520; //incrementing the multiples of 2510, which is divisible by 1-10
+  boolean isEven = false;
+
+  for (int count = 11; count <= 20; count++) {
+    if (multiple % count == 0) {
+      isEven = true;
+    } else {
+      isEven = false;
+      break;
+    }
+  }
+  
+  if (isEven) isMultiple = true;
+}
+
+System.out.println(multiple);
