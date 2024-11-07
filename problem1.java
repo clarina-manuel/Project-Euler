eb8e550ce62d16476de2cd02e515c1356bc34545
clarina-manuel <clarina.t.manuel@gmail.com>
2024-11-07 15:16:00 -0800

    Solved problem1

--- problem1.java
+++ problem1.java
@@ -1,2 +1,15 @@
 //Multiples of 3 or 5
 
+int count = 1;
+int sum = 0;
+
+while(count < 1000){
+  if ((count % 3 == 0) || (count % 5 == 0)) {
+    sum += count;
+    count++;
+  } else {
+    count++;
+  }
+}
+
+System.out.println(sum);
