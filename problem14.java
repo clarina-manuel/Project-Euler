87ce6f22df4783c2ac75d75315c3e3b3e54c5385
clarina-manuel <clarina.t.manuel@gmail.com>
2024-11-08 10:29:13 -0800

    Solved #14

--- problem14.java
+++ problem14.java
@@ -1,9 +1,26 @@
 //Collatz Sequence
-
-if (currentNum % 2 == 0) {
-  currentNum /= 2;
-} else {
-  currentNum = 3 * currentNum + 1;
+public static void main(String[] args){
+  int maxSequence = 0;
+  int maxStart = 0;
+  
+  for (int count = 1; count < 1000000; count ++){
+    if (collatzSequence(count) > maxSequence){
+      maxSequence = collatzSequence(count);
+      maxStart = count;
+    }
+  }
+  System.out.println(maxSequence);
+  System.out.println(maxStart);
 }
 
-chainLength++;
+//helper method to check for sequence
+public static int collatzSequence(int startingNum) {
+  long startingNumLong = startingNum; //use the long data type so it can hold all the digits
+  int chainLength = 1;
+  while (startingNum > 1){
+    if (startingNum % 2 == 0) startingNum /= 2;
+    else startingNum = ((3 * startingNum) + 1);
+    chainLength++; 
+  }
+  return chainLength;
+}
