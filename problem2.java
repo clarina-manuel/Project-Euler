be4f580e366c6f3e13318a1635c7dee94f0b7c7c
clarina-manuel <clarina.t.manuel@gmail.com>
2024-11-11 14:28:05 -0800

    Add problem description

--- problem2.java
+++ problem2.java
@@ -1,4 +1,6 @@
 //Even Fibonacci Numbers
+//By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+//https://projecteuler.net/problem=2
 
 int num1 = 1;
 int num2 = 2;
@@ -7,9 +9,8 @@ int sum = 2; //for the first even number
 
 while (nextNum < 4000000){
   nextNum = num1 + num2;
-  if(nextNum % 2 == 0){
-    sum += nextNum;
-  }
+  if(nextNum % 2 == 0) sum += nextNum;
+  
   num1 = num2;
   num2 = nextNum;
 }
