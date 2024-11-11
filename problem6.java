3d9c6d5fae8accc5217d6807779e4b3ed70ed09c
clarina-manuel <clarina.t.manuel@gmail.com>
2024-11-11 14:37:43 -0800

    Add problem description

--- problem6.java
+++ problem6.java
@@ -1,4 +1,6 @@
 //Sum Square Difference
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+//https://projecteuler.net/problem=6
 
 int sumOfSquares = 0;
 int squareofSum = 0;
