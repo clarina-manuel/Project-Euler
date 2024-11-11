a5437c5cb68f2cca93c3955c1af90c948c3ebef7
clarina-manuel <clarina.t.manuel@gmail.com>
2024-11-11 14:27:04 -0800

    Add problem description

--- problem1.java
+++ problem1.java
@@ -1,15 +1,12 @@
-//Multiples of 3 or 5
+//Find the sum of all the multiples of 3 or 5 below 1000. 
+//https://projecteuler.net/problem=1
 
 int count = 1;
 int sum = 0;
 
 while(count < 1000){
-  if ((count % 3 == 0) || (count % 5 == 0)) {
-    sum += count;
-    count++;
-  } else {
-    count++;
-  }
+  if ((count % 3 == 0) || (count % 5 == 0)) sum += count;
+  count++;
 }
 
 System.out.println(sum);
